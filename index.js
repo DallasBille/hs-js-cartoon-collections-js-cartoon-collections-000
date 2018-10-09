30301b4e051100fb9ba97055c6c5b9b262efc184
Dallas <dallasbille@gmail.com>
2018-10-09 22:46:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 function dwarfRollCall(dwarves) {
+  var dwarfLine=[];
+
+  for(let i=0; i<dwarves.length; i++){
+    dwarfLine.push(`${i+1}. ${dwarves[i]}`)
+  }
+  return dwarfLine.join(' ')
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  for(let i=0; i<planeteerCalls.length; i++){
+    
+  }
 }
 
 function longPlaneteerCalls(words) {
